4b70e63cecc9bfe9fd8ecf5c42fdeded538b8949
Tynahan <nusilite@gmail.com>
2025-05-30 15:07:20 +0900

    remove introduction section

---
+++ day2/2_what_is_value/assignment/script.js
@@ -0,0 +1,7 @@
+console.log("speed run error")
+
+const submitBtn = document.getElementById("submitBtn")
+submitBtn.addEventListener("click", function() {
+    // console.log("submitted!")
+    alert()
+})
