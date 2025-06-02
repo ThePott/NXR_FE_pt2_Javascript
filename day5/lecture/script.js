c3b700365ce24a1ee308aa691c1e69736e4bc30e
Tynahan <nusilite@gmail.com>
2025-06-02 18:13:52 +0900

    todo list done

---
+++ day5/lecture/script.js
@@ -0,0 +1,8 @@
+const addButton = document.getElementById("add-button")
+
+addButton.addEventListener("click", () => {
+    // window.localStorage.setItem("testKey", "testValueString")
+    // console.log("---- clicked")
+    localStorage.clear()
+})
+
